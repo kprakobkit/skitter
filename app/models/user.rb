5f8ef5764f87e09cc3c849395c0852a8710f9946
Mark Hoyt & shakrah yves <pair+markhoyt84.syves@devbootcamp.com>
2014-10-09 16:21:15 -0700

    create migration and model files for followers and followees

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ require 'bcrypt'
 class User < ActiveRecord::Base
   include BCrypt
   validates :username, uniqueness: true, presence: true
+
   has_many :skeets
+  has_many :followers, through: :follower_follows, source: :follower
+  has_many :follower_follows, foreign_key: :followee_id, class_name: "Follow"
+  has_many :followees, through: :followee_follows, source: :followee
+  has_many :followee_follows, foreign_key: :follower_id, class_name: "Follow"
 
   def password
     @password ||= Password.new(password_hash)
