430a67f2ba5ec5745a4c05ced6c9dcb41b036495
apprentice <achiu8@gmail.com>
2014-10-10 16:26:27 -0700

    added retweet

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 10.times do
-  user = User.create(username: Faker::Internet.user_name, password: "password")
+  user = User.create(
+    username: Faker::Internet.user_name,
+    password: "password",
+    name: Faker::Name.first_name,
+    state: Faker::Address.state,
+    city: Faker::Address.city,
+    email: "default@email.com",
+    bio: "default bio info"
+  )
+
   5.times { user.skeets.create(skeet: Faker::Company.catch_phrase) }
 end
 
